698983c9d4b552b5d0c3e4a63fd3a62952f07234
rochirino <rosarioaidachirino@gmail.com>
2025-08-31 18:00:51 -0300

    Update Prestamo.java

--- Biblioteca/src/Main/Java/Prototype/Prestamo.java
+++ Biblioteca/src/Main/Java/Prototype/Prestamo.java
@@ -1,4 +1,27 @@
 package Main.Java.Prototype;
 
-public class Prestamo {
+public class Prestamo implements Cloneable {
+    private String socio;
+    private int cantDias;
+
+
+    public Prestamo(String socio, int cantDias){
+        this.socio=socio;
+        this.cantDias=cantDias;
+    }
+     
+    @Override
+
+    public Prestamo clone(){
+        try{
+            return (Prestamo) super.clone();
+        }
+    catch (CloneNotSupportedException e){
+        throw new RuntimeException(e);
+    }
 }
+       public void mostrar(){
+        System.out.println("El socio: "+ socio + " pidio prestado un libro durante "+ cantDias +" Dias");
+       }
+}
+
