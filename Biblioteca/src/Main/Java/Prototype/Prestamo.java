06207567f2cc1b7c693e5a4c3918443bc90533a8
rochirino <rosarioaidachirino@gmail.com>
2025-09-02 17:57:28 -0300

    Update Prestamo.java

--- Biblioteca/src/Main/Java/Prototype/Prestamo.java
+++ Biblioteca/src/Main/Java/Prototype/Prestamo.java
@@ -1,27 +1,31 @@
-package Main.Java.Prototype;
+package Prototype;
+
+import java.time.LocalDateTime;
 
 public class Prestamo implements Cloneable {
-    private String socio;
-    private int cantDias;
+    private String libro;
+    private String usuario;
+    private LocalDateTime fechaHoraInicio;
+    private LocalDateTime fechaHoraFin;
 
 
-    public Prestamo(String socio, int cantDias){
-        this.socio=socio;
-        this.cantDias=cantDias;
+
+    public Prestamo(String libro, String usuario, LocalDateTime fechaHoraInicio, LocalDateTime fechaHoraFin) {
+        this.libro = libro;
+        this.usuario = usuario;
+        this.fechaHoraInicio = fechaHoraInicio;
+        this.fechaHoraFin = fechaHoraFin;
     }
-     
-    @Override
 
+    @Override
     public Prestamo clone(){
         try{
             return (Prestamo) super.clone();
+        }catch(CloneNotSupportedException e){
+            throw new RuntimeException(e);
         }
-    catch (CloneNotSupportedException e){
-        throw new RuntimeException(e);
     }
+ public void mostrar(){
+     System.out.println("el libro: "+ libro+ "sera prestado al Usuario: "+usuario+ "hasta: "+ fechaHoraFin);
+ }
 }
-       public void mostrar(){
-        System.out.println("El socio: "+ socio + " pidio prestado un libro durante "+ cantDias +" Dias");
-       }
-}
-
